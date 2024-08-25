fbc844b94699138367507bc4c82b169e9b9294ce
parham <p.mahdizadeh@khodro45.com>
2024-08-26 01:50:05 +0330

    endpoint
    wrote endpoint for search engine

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from database.query_builder import QueryBuilder
+from model.model import EmbeddingModels
+from sentence_transformers import SentenceTransformer
 
 app = FastAPI()
+model = SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class SentenceSerializer(BaseModel):
+    sentence: str
+
+
+@app.post("/search")
+async def search(sentence: SentenceSerializer):
+    # here should convert to vector embedding and compare to database
+
+    encoded_sentence = model.encode(sentence)
+
+    # finding closest to sentence that was given as input logic
+    query = QueryBuilder()
+    query.select()
+
+    return {f"to "}
 
 
 @app.get("/hello/{name}")
